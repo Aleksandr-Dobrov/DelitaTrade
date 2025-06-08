0ac45c50c17f4a7d166a834e34ebcf24dab2a2ac
Dobrov Aleksandr <AleksandrDobrov@outlook.com>
2025-06-08 18:02:02 +0300

    Create global date format constant.

---
+++ DelitaTrade.Common/Constants/FormatConstant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelitaTrade.Common.Constants
+{
+    public class FormatConstant
+    {
+        public class DateTimeFormat
+        {
+            public const string DayReportHeader = "dd.MM.yyyy";
+        }
+    }
+}
