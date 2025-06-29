2be7ff00e5f7a8e1a7e9b545d591023817d3f709
Dobrov Aleksandr <aleksandr_dobrov@abv.bg>
2025-06-29 09:15:10 +0300

    Add ItemId tag helper.

---
+++ DelitaTrade.WebApp/TagHelpers/DataItemIdTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DelitaTrade.WebApp.TagHelpers
+{
+    [HtmlTargetElement("input")]
+    public class DataItemIdTagHelper : TagHelper
+    {
+        public string? ItemId { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context.AllAttributes.TryGetAttribute("item-id", out var itemIdAttribute))
+            {
+                output.Attributes.SetAttribute("item-id", ItemId);
+            }
+        }
+    }
+}
