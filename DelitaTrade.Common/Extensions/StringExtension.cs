85620cae590770773623b3a8caa11322b4d2ff71
Dobrov Aleksandr <aleksandr_dobrov@abv.bg>
2025-07-08 15:36:14 +0300

    Add extention method to remove controller word from controller name.

---
+++ DelitaTrade.Common/Extensions/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace DelitaTrade.Common.Extensions
+{
+    public static class StringExtension
+    {
+        public static string GetControllerName(this string controller)
+        {
+            if (string.IsNullOrEmpty(controller))
+            {
+                return string.Empty;
+            }
+            if (controller.EndsWith("Controller"))
+            {
+                return controller.Substring(0, controller.Length - "Controller".Length);
+            }
+            return controller;
+        }
+    }
+}
