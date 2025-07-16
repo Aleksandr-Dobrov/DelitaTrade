ba16c9bfae5a81f52284d2f49a98609eac9fcf39
Dobrov Aleksandr <aleksandr_dobrov@abv.bg>
2025-07-16 12:21:40 +0300

    Add PaymentViewModel comparer.

---
+++ DelitaTrade.Core/Comparers/PaymentViewModelEqualComparer.cs
@@ -0,0 +1,18 @@
+﻿using DelitaTrade.Core.ViewModels.DeliveryModels;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DelitaTrade.Core.Comparers
+{
+    public class PaymentViewModelEqualComparer : IEqualityComparer<PaymentViewModel>
+    {
+        public bool Equals(PaymentViewModel? x, PaymentViewModel? y)
+        {
+            return x != null && y != null && x.InvoiceNumber == y.InvoiceNumber;
+        }
+
+        public int GetHashCode([DisallowNull] PaymentViewModel obj)
+        {
+            return obj.InvoiceNumber.GetHashCode();
+        }
+    }
+}
