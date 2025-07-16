d7bdd952667f9c01b3776d7a2b583caa2a6b8a57
Dobrov Aleksandr <aleksandr_dobrov@abv.bg>
2025-07-16 12:20:07 +0300

    Add base service with logic to check user roles.

---
+++ DelitaTrade.Core/Services/BaseService.cs
@@ -0,0 +1,43 @@
+﻿using DelitaTrade.Core.ViewModels;
+
+namespace DelitaTrade.Core.Services
+{
+    public class BaseService
+    {
+        protected static bool IsAtLeastInOneRole(UserViewModel user, params string[] roles)
+        {
+            bool result = false;
+
+            foreach (string role in roles) 
+            {
+                if (user.Roles.Contains(role)) 
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        protected static bool IsInAllRoles(UserViewModel user, params string[] roles)
+        {
+            bool result = false;
+
+            foreach (string role in roles)
+            {
+                if (user.Roles.Contains(role))
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
