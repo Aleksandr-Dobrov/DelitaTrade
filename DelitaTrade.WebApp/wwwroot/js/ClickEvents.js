7049def432eaea6c0d65c2912b45be0ad4c77c48
Dobrov Aleksandr <aleksandr_dobrov@abv.bg>
2025-07-26 11:07:56 +0300

    Add delivery filter to day report details.

---
+++ DelitaTrade.WebApp/wwwroot/js/ClickEvents.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('DOMContentLoaded', ChangeClassOnClic(oldClassName, newClassName));
+
+function ChangeClassOnClic(oldClassName, newClassName) {
+    const itemEl = document.querySelectorAll('.' + oldClassName);
+    itemEl.forEach(function (item) {
+        item.addEventListener('click', function () {
+            item.classList.remove(oldClassName)
+            item.classList.toggle(newClassName);
+        });
+    });
+}
---
+++ DelitaTrade.WebApp/wwwroot/js/DayReport/DayReportFilter.js
@@ -0,0 +1,50 @@
+﻿document.addEventListener('DOMContentLoaded', initiateFilter)
+
+function initiateFilter(e) {
+    const filterArg = document.querySelector('#filter-arg');
+    const filteredElements = document.querySelectorAll('.day-report-list-container');
+    if (filterArg == null) return;
+    filteredObjects(filterArg.value, filteredElements);
+    
+    filterArg.addEventListener('input', myfunction);
+}
+
+function myfunction(e) {
+    const filterArg = document.querySelector('#filter-arg');
+    const filteredElements = document.querySelectorAll('.day-report-list-container');
+    filteredObjects(filterArg.value, filteredElements);
+}
+
+function filteredObjects(filterArg, filteredElement) {
+    if (filterArg == 'All') {
+        filteredElement.forEach((el) => {
+            setVisible(el);
+        });
+    }
+    else if (filterArg == 'Completed') {
+        document.querySelectorAll(completed).forEach((el) => {
+            setVisible(el);
+        });
+
+        document.querySelectorAll(notCompleted).forEach((el) => {
+            setHide(el);
+        });
+    }
+    else if (filterArg == 'Not completed') {
+        document.querySelectorAll(completed).forEach((el) => {
+            setHide(el);
+        });
+
+        document.querySelectorAll(notCompleted).forEach((el) => {
+            setVisible(el);
+        });
+    }
+}
+
+function setVisible(el) {
+    el.style.display = "block";
+}
+
+function setHide(el) {
+    el.style.display = "none";
+}
