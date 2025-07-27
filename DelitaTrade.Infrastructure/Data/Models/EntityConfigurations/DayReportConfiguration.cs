ca8f9eb2d8fa31e9f13b0d2878ed6e172a8f3eac
Dobrov Aleksandr <aleksandr_dobrov@abv.bg>
2025-07-27 19:40:41 +0300

    Add ValueComparer to DayReport.Banknote Dictionary<decimal, int> property.

--- DelitaTrade.Infrastructure/Data/Models/EntityConfigurations/DayReportConfiguration.cs
+++ DelitaTrade.Infrastructure/Data/Models/EntityConfigurations/DayReportConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Newtonsoft.Json;
 using System.Text.Json;
@@ -8,10 +9,16 @@ namespace DelitaTrade.Infrastructure.Data.Models.EntityConfigurations
     public class DayReportConfiguration : IEntityTypeConfiguration<DayReport>
     {
         public void Configure(EntityTypeBuilder<DayReport> builder)
-        {            
+        {     
+            var dictionaryComparer = new ValueComparer<Dictionary<decimal, int>>(
+                (x, y) => x.SequenceEqual(y),
+                x => x.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                x => x.ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
+
             builder.Property(d => d.Banknotes)
                 .HasConversion(v => JsonConvert.SerializeObject(v),
-                                 v => JsonConvert.DeserializeObject<Dictionary<decimal,int>>(v));
+                                 v => JsonConvert.DeserializeObject<Dictionary<decimal,int>>(v))
+                .Metadata.SetValueComparer(dictionaryComparer);
         }
     }
 }
