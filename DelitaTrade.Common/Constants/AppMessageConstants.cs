4f3da1421d5d1e32af9b6ff842bdecc948096fd0
Dobrov Aleksandr <aleksandr_dobrov@abv.bg>
2025-08-02 20:54:45 +0300

    Add Toastr notifications.

---
+++ DelitaTrade.Common/Constants/AppMessageConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace DelitaTrade.Common.Constants
+{
+    public class AppMessageConstants
+    {
+        public const string Info = "InfoMessage";
+        public const string Warning = "WarningMessage";
+        public const string Error = "ErrorMessage";
+        public const string Success = "SuccessMessage";
+    }
+}
---
+++ DelitaTrade.WebApp/wwwroot/js/notification.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener('DOMContentLoaded', initNotification);
+
+function initNotification(e) {
+    infoEl = document.querySelector('#info-message');
+    successEl = document.querySelector('#success-message');
+    warningEl = document.querySelector('#warning-message');
+    errorEl = document.querySelector('#error-message');
+
+    if (infoEl && infoEl.value) {
+        toastr.info(infoEl.value);
+    }
+    if (successEl && successEl.value) {
+        toastr.success(successEl.value);
+    }
+    if (warningEl && warningEl.value) {
+        toastr.warning(warningEl.value);
+    }
+    if (errorEl && errorEl.value) {
+        toastr.error(errorEl.value);
+    }
+}
