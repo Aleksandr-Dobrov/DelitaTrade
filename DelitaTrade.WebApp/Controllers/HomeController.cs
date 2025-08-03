608e75430c80bd10e8fbf783fd342398d8f4be97
Dobrov Aleksandr <aleksandr_dobrov@abv.bg>
2025-08-03 13:25:35 +0300

    Implement custom error pages whit redirection for 404, 401, 403 status code.

--- DelitaTrade.WebApp/Controllers/HomeController.cs
+++ DelitaTrade.WebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using DelitaTrade.WebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,6 +8,11 @@ namespace DelitaTrade.WebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Dictionary<int, string> _errorPages = new()
+        {
+            { 404, "NotFoundError" },
+            { 401, "UnauthorizedError" }
+        };
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -18,15 +24,25 @@ namespace DelitaTrade.WebApp.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
-        {
+        {            
             return View();
         }
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int? statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+           
+            if (statusCode.HasValue && _errorPages.TryGetValue(statusCode.Value, out string? value))
+            {               
+                return View(value);
+            }
+            else
+            {
+                ViewData["StatusCode"] = statusCode;
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
     }
 }
--- DelitaTrade.WebApp/Program.cs
+++ DelitaTrade.WebApp/Program.cs
@@ -48,6 +48,9 @@ namespace DelitaTrade.WebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
+
             app.UseStaticFiles();
 
             app.UseHttpsRedirection();
