3148ba9ae756b0e25606a8acad3dce8ab67de5ce
Dobrov Aleksandr <aleksandr_dobrov@abv.bg>
2025-08-06 11:57:28 +0300

    Remove Unusing code.

--- DelitaTrade.Core/Contracts/IProductService.cs
+++ DelitaTrade.Core/Contracts/IProductService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DelitaTrade.Core.ViewModels;
+﻿using DelitaTrade.Core.ViewModels;
 using DelitaTrade.Core.ViewModels.ProductsManagementModels;
 
 namespace DelitaTrade.Core.Contracts
