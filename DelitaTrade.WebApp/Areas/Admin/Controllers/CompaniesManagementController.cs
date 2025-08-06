a7244d8fa6d9c0027ebbf5b4e6d306c8a0b3a94e
Dobrov Aleksandr <aleksandr_dobrov@abv.bg>
2025-08-06 14:44:10 +0300

    Add CompaniesManagementController (not implemented).

---
+++ DelitaTrade.WebApp/Areas/Admin/Controllers/CompaniesManagementController.cs
@@ -0,0 +1,16 @@
+﻿using DelitaTrade.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using static DelitaTrade.Common.Constants.AppMessageConstants;
+
+namespace DelitaTrade.WebApp.Areas.Admin.Controllers
+{
+    public class CompaniesManagementController(UserManager<DelitaUser> userManager) : BaseAdminController(userManager)
+    {
+        public IActionResult Index()
+        {
+            TempData[Info] = "Sorry, the company management is not yet implemented. For import companies and objects try to import payments to day report from json file. This will add automatically not existing companies an objects to data base.";
+            return NotFound();
+        }
+    }
+}
